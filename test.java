3579b27b6b57359227beb06f488c784d005bc9b7
joseph <josephsanthosh121@gmail.com>
2024-12-09 11:07:43 +0530

    done programming

---
+++ test.java
@@ -0,0 +1,12 @@
+public class test{
+    public static void main(String args[]){
+        int num1=5;
+        int num2=10;
+        int sum=num1+num2;
+        int mul=num1*num2;
+        double div=num1/num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+        System.out.println("The product of " + num1 + " and "  + num2 + " is " + mul);
+        System.out.println("The result of " + num1 + "/" + num2 +" is " + div);
+    }
+}
